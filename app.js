4457ccd37d71c88c9386a81136eb1d6479790408
n1colassilva <91223377+n1colassilva@users.noreply.github.com>
2022-11-04 10:40:10 -0300

    Add files via upload
    links adicionados, link para primeiro projeto linkado, erro gramatico corrigido

---
+++ app.js
@@ -0,0 +1,11 @@
+function copyEmail() {
+    navigator.clipboard.writeText
+        ("nicolassilva0109@gmail.com");
+    alert("Email copiado!")
+}
+
+function copyPhone() {
+    navigator.clipboard.writeText
+        ("+55 (34) 9 9199-9412");
+    alert("Numero copiado!")
+}
