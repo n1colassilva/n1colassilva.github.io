b52b955357d076026e5518585caafd2f95a6e015
n1colassilva <91223377+n1colassilva@users.noreply.github.com>
2023-03-06 11:08:27 -0300

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -6,11 +6,29 @@ const rightButtonClick = () => {
 
     // Bump active index
 
-    const nextCarrouselIndex = activeIndex + 1 <= carrouselItems.length - 1 ? activeIndex + 1 : 0;
+    const nextCarrouselIndex = carrouselIndex + 1 <= carrouselItems.length - 1 ? carrouselIndex + 1 : 0;
 
-    const currentCarrouselItem = document.querySelector(`[data-index="${activeIndex}"]`),
+    const currentCarrouselItem = document.querySelector(`[data-index="${carrouselIndex}"]`),
         nextCarrouselItem = document.querySelector(`[data-index="${nextCarrouselIndex}"]`);
-    
+
+    // Active item becomes after
+
+    currentCarrouselItem.dataset.status = "after";
+
+    // Next item becomes active 
+
+    nextCarrouselItem.dataset.status = "active"
+}
+
+const leftButtonClick = () => {
+
+    // Bump active index
+
+    const nextCarrouselIndex = carrouselIndex - 1 <= carrouselItems.length - 1 ? carrouselIndex + 1 : 0;
+
+    const currentCarrouselItem = document.querySelector(`[data-index="${carrouselIndex}"]`),
+        nextCarrouselItem = document.querySelector(`[data-index="${nextCarrouselIndex}"]`);
+
     // Active item becomes after
 
     currentCarrouselItem.dataset.status = "after";
