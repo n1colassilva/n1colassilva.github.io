b4472c999082608360e769729e55f75fc73c40bc
n1colassilva <nicolassilva0109@gmail.com>
2023-05-03 20:58:41 -0300

    added backup plan in case no language is not available
    Signed-off-by: n1colassilva <nicolassilva0109@gmail.com>

--- javascript/language_redirect.js
+++ javascript/language_redirect.js
@@ -3,5 +3,7 @@ const userLang = navigator.language || navigator.userLanguage;
 if (userLang.startsWith("en")) {
 	window.location.href = "../docs/pages/en/en.html";
 } else if (userLang.startsWith("pt")) {
-	window.location.href = "../docs/pt/pt.html";
+	window.location.href = "../docs/pages/pt/pt.html";
+} else {
+	window.location.href = "../docs/pages/en/en.html";
 }
