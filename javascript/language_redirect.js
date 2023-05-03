685d1a5149d12f48a585037f6e07d46d72048648
n1colassilva <nicolassilva0109@gmail.com>
2023-05-03 20:56:14 -0300

    fixed(?) language redirect directories
    Signed-off-by: n1colassilva <nicolassilva0109@gmail.com>

--- javascript/language_redirect.js
+++ javascript/language_redirect.js
@@ -1,7 +1,7 @@
 const userLang = navigator.language || navigator.userLanguage;
 
 if (userLang.startsWith("en")) {
-	window.location.href = "pages/en/en.html";
+	window.location.href = "../docs/pages/en/en.html";
 } else if (userLang.startsWith("pt")) {
-	window.location.href = "pages/pt/pt.html";
+	window.location.href = "../docs/pt/pt.html";
 }
