a214d29849352eb1f593057e953f9ade808a5220
n1colassilva <nicolassilva0109@gmail.com>
2023-05-03 09:14:56 -0300

    fixed file pathing
    Signed-off-by: n1colassilva <nicolassilva0109@gmail.com>

--- javascript/language_redirect.js
+++ javascript/language_redirect.js
@@ -1,7 +1,7 @@
 const userLang = navigator.language || navigator.userLanguage;
 
 if (userLang.startsWith("en")) {
-	window.location.href = "/pages/en/";
+	window.location.href = "pages/en/en.html";
 } else if (userLang.startsWith("pt")) {
-	window.location.href = "/pages/pt/";
+	window.location.href = "pages/pt/pt.html";
 }
