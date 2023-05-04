43e32fcedff58ffe32da71eb5706229fb67d56dd
nicolas paratudo <nicolassilva0109@gmail.com>
2023-05-04 11:05:26 -0300

    fixed redirect error for pt-br, added nojekyll
    Signed-off-by: nicolas paratudo <nicolassilva0109@gmail.com>

--- javascript/language_redirect.js
+++ javascript/language_redirect.js
@@ -3,7 +3,7 @@ const userLang = navigator.language || navigator.userLanguage;
 if (userLang.startsWith("en")) {
 	window.location.href = "/pages/en/en.html";
 } else if (userLang.startsWith("pt")) {
-	window.location.href = "/pages/pt/pt.html";
+	window.location.href = "/pages/pt-br/pt-br.html";
 } else {
 	window.location.href = "/pages/en/en.html";
 }
