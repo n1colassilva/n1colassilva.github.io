2e870738ff27dea132b35fe99624513c6ddd37e8
Nícolas Paratudo <nicolassilva0109@gmail.com>
2023-05-05 11:13:33 -0300

    added arrow key navigation
    Signed-off-by: Nícolas Paratudo <nicolassilva0109@gmail.com>

--- src/javascript/carousel.js
+++ src/javascript/carousel.js
@@ -19,70 +19,71 @@ const carouselItems = document.getElementsByClassName("info-carousel-item");
 let carouselIndex = 0;
 
 const rightButtonClick = () => {
+	//determining what is the previous index and its element
+	//(if current-1 < 0 then its the last one, else its just current -1)
+	const previousIndexNumber = carouselIndex - 1 < 0 ? carouselItems.length - 1 : carouselIndex - 1;
+	const previousIndex = document.querySelector(`[data-index="${previousIndexNumber}"]`);
 
+	//current index is just taken from the global variable
+	//later on its updated, thats why we can rely on it
+	const currentIndex = document.querySelector(`[data-index="${carouselIndex}"]`);
 
-    //determining what is the previous index and its element 
-    //(if current-1 < 0 then its the last one, else its just current -1)
-    const previousIndexNumber = carouselIndex - 1 < 0 ? carouselItems.length - 1 : carouselIndex - 1;
-    const previousIndex = document.querySelector(`[data-index="${previousIndexNumber}"]`);
+	// if the next + 1 < the highest index then the next is just the current + 1, if not then it loops back to 0
+	const nextIndexNumber = carouselIndex + 1 < carouselItems.length ? carouselIndex + 1 : 0;
+	const nextIndex = document.querySelector(`[data-index="${nextIndexNumber}"]`);
 
+	const nextNextIndexNumber = nextIndexNumber + 1 <= carouselItems.length - 1 ? nextIndexNumber + 1 : 0;
+	const nextNextIndex = document.querySelector(`[data-index="${nextNextIndexNumber}"]`);
 
-    //current index is just taken from the global variable
-    //later on its updated, thats why we can rely on it
-    const currentIndex = document.querySelector(`[data-index="${carouselIndex}"]`);
+	previousIndex.dataset.status = "unknown";
+	currentIndex.dataset.status = "previous-from-current";
+	nextIndex.dataset.status = "current";
+	nextNextIndex.dataset.status = "next";
 
-
-    // if the next + 1 < the highest index then the next is just the current + 1, if not then it loops back to 0
-    const nextIndexNumber = carouselIndex + 1 < carouselItems.length ? carouselIndex + 1 : 0;
-    const nextIndex = document.querySelector(`[data-index="${nextIndexNumber}"]`);
-
-
-    const nextNextIndexNumber = nextIndexNumber + 1 <= carouselItems.length - 1 ? nextIndexNumber + 1 : 0;
-    const nextNextIndex = document.querySelector(`[data-index="${nextNextIndexNumber}"]`);
-
-
-    previousIndex.dataset.status = "unknown";
-    currentIndex.dataset.status = "previous-from-current"
-    nextIndex.dataset.status = "current"
-    nextNextIndex.dataset.status = "next"
-
-    // Update the current index variable
-    carouselIndex = nextIndexNumber;
-}
+	// Update the current index variable
+	carouselIndex = nextIndexNumber;
+};
 
 // similar  code as rightButtonClick, but change the status because we are going back
 // and want it to always come from the correct direction
 const leftButtonClick = () => {
-
-
-    /*
+	/*
     determining what is the previous index and its element 
     (if current-1 < 0 then its the last one, else its just current -1)
     */
-    const previousIndexNumber = carouselIndex - 1 < 0 ? carouselItems.length - 1 : carouselIndex - 1;
-    const previousIndex = document.querySelector(`[data-index="${previousIndexNumber}"]`);
+	const previousIndexNumber = carouselIndex - 1 < 0 ? carouselItems.length - 1 : carouselIndex - 1;
+	const previousIndex = document.querySelector(`[data-index="${previousIndexNumber}"]`);
 
+	const previousPreviousIndexNumber = previousIndexNumber - 1 < 0 ? carouselItems.length - 1 : previousIndexNumber - 1;
+	const previousPreviousIndex = document.querySelector(`[data-index="${previousPreviousIndexNumber}"]`);
 
-    const previousPreviousIndexNumber = previousIndexNumber - 1 < 0 ? carouselItems.length - 1 : previousIndexNumber - 1;
-    const previousPreviousIndex = document.querySelector(`[data-index="${previousPreviousIndexNumber}"]`);
+	//current index is just taken from the global variable
+	//later on its updated, thats why we can rely on it
+	const currentIndex = document.querySelector(`[data-index="${carouselIndex}"]`);
+	// console.log(`current index: ${currentIndex.dataset.index}`);
 
+	// if the next + 1 < the highest index then the next is just the current + 1, if not then it loops back to 0
+	const nextIndexNumber = carouselIndex + 1 < carouselItems.length ? carouselIndex + 1 : 0;
+	const nextIndex = document.querySelector(`[data-index="${nextIndexNumber}"]`);
 
-    //current index is just taken from the global variable
-    //later on its updated, thats why we can rely on it
-    const currentIndex = document.querySelector(`[data-index="${carouselIndex}"]`);
-    console.log(`current index: ${currentIndex.dataset.index}`);
+	previousPreviousIndex.dataset.status = "previous";
+	previousIndex.dataset.status = "current";
+	currentIndex.dataset.status = "next-from-current";
+	nextIndex.dataset.status = "unknown";
 
+	// Update the current index variable
+	carouselIndex = previousIndexNumber;
+};
 
-    // if the next + 1 < the highest index then the next is just the current + 1, if not then it loops back to 0
-    const nextIndexNumber = carouselIndex + 1 < carouselItems.length ? carouselIndex + 1 : 0;
-    const nextIndex = document.querySelector(`[data-index="${nextIndexNumber}"]`);
-
-
-    previousPreviousIndex.dataset.status = "previous"
-    previousIndex.dataset.status = "current"
-    currentIndex.dataset.status = "next-from-current"
-    nextIndex.dataset.status = "unknown"
-
-    // Update the current index variable
-    carouselIndex = previousIndexNumber;
+function arrowNavHandler(event) {
+	// check if the pressed key is an arrow key
+	if (event.key === "ArrowLeft") {
+		// call leftButtonClick function for left arrow
+		leftButtonClick();
+	} else if (event.key === "ArrowRight") {
+		// call rightButtonClick function for right arrow
+		rightButtonClick();
+	}
 }
+
+document.addEventListener("keydown", arrowNavHandler);
